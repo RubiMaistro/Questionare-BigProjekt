6d231f0d319418ec1c1e39c09191cdcf4e61ed47
DZoltan <55488265+DZoltan@users.noreply.github.com>
2021-11-03 10:54:09 +0100

    adadtbázis kapcsolatok

--- Laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ Laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('isDev')->nullable();
+            $table->boolean('isAsker')->nullable();
+            $table->boolean('isAnswerer')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
