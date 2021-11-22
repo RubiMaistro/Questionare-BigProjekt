26bee53363c560454374d75dd5b193994ee20118
flipascot <tbanfalvi2000@gmail.com>
2021-11-22 12:55:18 +0100

    designe for start lap

--- Laravel/resources/views/questionare/questionare.blade.php
+++ Laravel/resources/views/questionare/questionare.blade.php
@@ -1,72 +1,155 @@
 @extends('layouts.app')
 
 @section('content')
+ <!--Main Navigation-->
+ <header>
+    <style>
+      #intro {
+        background-image: url("https://mdbootstrap.com/img/new/fluid/city/018.jpg");
+        height: 100vh;
+      }
+
+      /* Height for devices larger than 576px */
+      @media (min-width: 992px) {
+        #intro {
+          margin-top: -58.59px;
+        }
+      }
+
+      .navbar .nav-link {
+        color: #fff !important;
+      }
+    </style>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark d-none d-lg-block" style="z-index: 2000;">
+      <div class="container-fluid">
+        <!-- Navbar brand -->
+        <a class="navbar-brand nav-link" target="_blank">
+          <strong>ES</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarExample01"
+          aria-controls="navbarExample01" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarExample01">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item active">
+              <a class="nav-link" aria-current="page" href="#intro">Laravel</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#intro">Log Out</a>
+            </li>
+          </ul>
 
 
-<!Kártyák>
+         
+         
+          <!-- Navbar brand -->
 
-<div class="row row-cols-1 row-cols-md-2 g-4">
-  <div class="col">
-    <div class="card">
-     
-      <div class="card-body">
-        <h5 class="card-title">kérdés 1</h5>
+          <ul class="navbar-nav d-flex flex-row">
+            
+       
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Navbar -->
 
-        <div class="btn-group" role="group" aria-label="Basic example">
-  <button type="button" class="btn btn-primary">elso</button>
-  <button type="button" class="btn btn-primary">masodik</button>
-  <button type="button" class="btn btn-primary">harmadik</button>
+    <!-- Background image -->
+    <div id="intro" class="bg-image shadow-2-strong">
+      <div class="mask" style="background-color: rgba(0, 0, 0, 0.8);">
+        <div class="container d-flex align-items-center justify-content-center text-center h-100">
+          <!-- BEJELNETKEZÉST IDE -->
+
+        </div>
+      </div>
+    </div>
+    <!-- Background image -->
+  </header>
+  <!--Main Navigation-->
+
+  <!--Main layout-->
+  <main class="mt-5">
+    <div class="container">
+      <!--Section: Content-->
+      <section>
+        <div class="row">
+          <div class="col-md-6 gx-5 mb-4">
+            <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
+              <img src="https://mdbootstrap.com/img/new/slides/031.jpg" class="img-fluid" />
+              <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-md-6 gx-5 mb-4">
+            <h4><strong>Rólunk</strong></h4>
+            <p class="text-muted">
+             Ide mehet egy kis szöveg rólunk a weblapról kábítószer beszerzési útmutató ilyek
+            </p>
+            <p><strong>Kérdőívekről</strong></p>
+            <p class="text-muted">
+             blalalbalamblablabablbalblabla a kérdőívekről
+            </p>
+          </div>
+        </div>
+      </section>
+      <!--Section: Content-->
+
+      <hr class="my-5" />
+
+      <!--Section: Content-->
+      <section class="mb-5">
+        <h4 class="mb-5 text-center"><strong>Regisztráció</strong></h4>
+
+        <div class="row d-flex justify-content-center">
+          <div class="col-md-6">
+            <form>
+            
+              <!-- Email input -->
+              <div class="form-outline mb-4">
+                <input type="email" id="form3Example3" class="form-control" />
+                <label class="form-label" for="form3Example3">Email address</label>
+              </div>
+
+              <!-- Password input -->
+              <div class="form-outline mb-4" >
+                <input type="password" id="form3Example4" class="form-control" />
+                <label class="form-label" for="form3Example4">Password</label>
+              </div>
+
+
+            
+              <button type="submit" class="btn btn-primary btn-block mb-4">
+                Regisztrálás
+              </button>
+    </form>
+
+
+ <!-- Kártya-->
+
+
+<div class=" row justify-content-center">
+
+<div class="card text-white bg-dark mb-3" style="max-width: 18rem;" >
+  <div class="card-header">1.</div>
+  <div class="card-body">
+    <h5 class="card-title">kérdése</h5>
+    <p class="card-text"><div class="btn-group-vertical" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-primary">Left</button>
+  <button type="button" class="btn btn-primary">Middle</button>
+  <button type="button" class="btn btn-primary">Right</button>
+</div>
+</p>
+  </div>
+</div>
 </div>
 
-        <p class="card-text">melyik jellemző önre</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-     
-      <div class="card-body">
-        <h5 class="card-title">kérdés 2</h5>
-        <ul class="list-group">
-  <li class="list-group-item active" aria-current="true">Mit szeretenél?</li>
-  <li class="list-group-item">elso</li>
-  <li class="list-group-item">masodik</li>
-  <li class="list-group-item">harmadik</li>
-  <li class="list-group-item">negyedik</li>
-</ul>
-        <p class="card-text">válaszon ki 1 et a listából</p>
-      </div>
-    </div>
-  </div>
+<div class=" row justify-content-center">
 
-  <div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="..." class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title"><--ide mehet 1 kép</h5>
-        <p class="card-text"></p>
-        <p class="card-text"><small class="text-muted">csak pozitív válasz elfogadható</small></p>
-      </div>
-    </div>
-  </div>
-</div>
 
-  <div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">   ^</h5>
-        <h5 class="card-title">   |</h5>
-        <h5 class="card-title">   |</h5>
-        <h5 class="card-title">vagy ide egy kép?</h5>
-        <p class="card-text">meg leírás</p>
-      </div>
-    </div>
-  </div>
-</div>
 
 <! oldal >
 <nav aria-label="Page navigation example">
@@ -82,5 +165,4 @@
     </li>
   </ul>
 </nav>
-
 @endsection
